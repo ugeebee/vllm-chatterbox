28673b25491e222091a27232462c4fb29265f941
ugeebee <ugbhartariya@gmail.com>
2026-08-14 03:19:39 +0530

    feat: call model registration function immediately upon plugin import

--- vllm_chatterbox/plugin.py
+++ vllm_chatterbox/plugin.py
@@ -4,3 +4,6 @@ def register():
     # Register the model class with vLLM
     from .model import T3TurboForCausalLM
     vllm.ModelRegistry.register_model("T3TurboForCausalLM", T3TurboForCausalLM)
+
+# Call it immediately when the module is imported
+register()
