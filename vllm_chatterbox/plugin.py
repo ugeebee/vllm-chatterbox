7349bc2a06fcc3edf2dfe66c8d79000f6eeb77e2
ugeebee <ugbhartariya@gmail.com>
2026-08-14 03:29:27 +0530

    fix: update registered model name to GPT2LMHeadModel in plugin registration

--- vllm_chatterbox/plugin.py
+++ vllm_chatterbox/plugin.py
@@ -3,7 +3,7 @@ import vllm
 def register():
     # Register the model class with vLLM
     from .model import T3TurboForCausalLM
-    vllm.ModelRegistry.register_model("T3TurboForCausalLM", T3TurboForCausalLM)
+    vllm.ModelRegistry.register_model("GPT2LMHeadModel", T3TurboForCausalLM)
 
 # Call it immediately when the module is imported
 register()
